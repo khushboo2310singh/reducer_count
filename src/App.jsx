ba1cff05fc381adeb2c85f2de9dda1cc36fffd66
khushbu <khushboo931@gmail.com>
2024-02-01 14:34:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,62 @@
+import React from 'react'
+import { useReducer } from 'react'
+const App = () => {
 
-import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
-import Products from './Products';
-import Home from './Home';
-import Cart from './Cart';
-import Wishlist from './Wishlist';
-import Head from './Head';
-import {useSelector} from 'react-redux';
-import Login from './Login'
-import Footer from './Footer'
-import Subtotal from './Subtotal';
-import SinglePro from './SinglePro';
-import CategoryJewel from './CategoryJewel';
-import CategoryMen from './CategoryMen';
-import CategoryWomen from './CategoryWomen';
-import CategoryElectro from './CategoryElectro';
-import Register from './Register';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Reset from './Reset';
-import Address from './Address'
-
-
-const App=()=>{
-    const count=useSelector(state=>state.cart.count);
-    const categories=useSelector(state=>state.categories.categories);
+  const mystyle={
+    theme:{
+      backgroundColor:"skyblue",
+      width:"100%",
+      height:"100vh"
+    },
+    intheme:{
+      backgroundColor:"brown",
+     color:"white",
+      height:"200px",
+      width:"400px"
+    }
+   
+   
+  }
+  const is={
+    count:0,
+  }
+  const reducer=(state, action)=>{
+    switch(action.type)
+    {
+      case "inc":
+        return {...state,count:state.count+1}
     
-    return(
-      <>
     
-       
-        <BrowserRouter>
-        <ToastContainer/>
-        <Head/>
-        
-      
-             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Products" element={<Products />} />
-                 <Route path="/Cart" element={<Cart />} /> 
-                <Route path="/Wishlist" element={<Wishlist />} />
-                <Route path="/Footer" element={<Footer />} />
-                <Route path="/Subtotal" element={<Subtotal />} />
-                 <Route path="/SinglePro/:id" element={<SinglePro />} />
-                <Route path="/CategoryJewel" element={<CategoryJewel />} />  
-                <Route path="/CategoryElectro" element={<CategoryElectro />} />  
-                <Route path="/CategoryMen" element={<CategoryMen />} />  
-                <Route path="/CategoryWomen" element={<CategoryWomen />} />  
-
-                <Route path="/Login" element={<Login />} />
-                <Route path="/Register" element={<Register/>} />
-                <Route path="/Reset" element={<Reset/>} />
-                <Route path="/Address" element={<Address/>} />
-                
-            </Routes>
-           
-         </BrowserRouter> 
-        
-        
-        </>
-    );
+      case "dec":
+        return {...state,count:state.count-1}
+      case "reset":
+        return is;
+     default:
+      return state;
+  }
 }
+  const[state,dispatch]=useReducer(reducer,is)
+  return (
+    <>
+    <div className='container-fluid ' >
+      <div className='row' style={mystyle.theme}>
+        <div className='d-flex justify-content-center align-items-center' >
+         
+          <div style={mystyle.intheme}>
+            
+             <h2 className='text-center'>count is {state.count}</h2>
+            <input type="button" value="Increment count" onClick={()=>dispatch({type:"inc"})} style={{backgroundColor:"black", color:"white"}}/>
+            <input type="button" value="Decrement count" onClick={()=>dispatch({type:"dec"})} style={{backgroundColor:"black", color:"white"}}/>
+            <input type="button" value="Reset count" onClick={()=>dispatch({type:"reset"})} style={{backgroundColor:"black", color:"white"}}/>
+          
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
 export default App;
-
-
--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import productsReducer from './productSlice';
-import cartReducer from './cartSlice';
-import wishlistReducer from './wishlistSlice';
-import authReducer from './authSlice'
-import categoriesReducer from './categoriesSlice'
-import { configureStore } from '@reduxjs/toolkit';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-const store= configureStore(
-    {
-        reducer:{
-            products:productsReducer,
-            cart:cartReducer,
-            wish:wishlistReducer,
-            categories:categoriesReducer,
-            auth:authReducer,
-            
-        }
-    }
-)
-
 const root=ReactDOM.createRoot(document.getElementById("root"));
-
-root.render( <Provider store={store}>
-    <App /></Provider>);
+// root.render(<App/>);
+root.render( <App />);
